169456b456c4b422ecf5b94e8700a05a20875cae
Cyrine G <syrynegamoudi@gmail.com>
2020-05-11 15:21:00 +0200

    react based website

--- roboFriends/src/Card.js
+++ roboFriends/src/Card.js
@@ -3,8 +3,8 @@ import React from 'react';
 const Card = (props) => {
 	const {name, email, id} = props;
 	return (
-		<div className='bg-light-green dib br3 pa3 na2 grow bw2 shadow-5'>
-			<img alt='robot' src={'https://robohash.org/{id}'} height ='150px' />
+		<div className='bg-light-green dib br3 pa3 space grow bw2 shadow-5'>
+			<img alt='robot' src={'https://robohash.org/{id}'} height ='150px' width='150' />
 			<div>
 				<h2> {name} </h2>
 				<p> {email} </p>
